c20e575ddcf50e97f44419d045965dfaa4031041
Mohd Saqueib Ansari <saquibweb@gmail.com>
2019-02-11 06:51:14 +0530

    controller can be swapped now

---
+++ src/SavesSettings.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace QCod\AppSettings;
+
+use Illuminate\Http\Request;
+use QCod\AppSettings\Setting\AppSettings;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+trait SavesSettings
+{
+    use ValidatesRequests;
+
+    /**
+     * Display the settings page
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $settingsUI = config('app_settings', []);
+        $settingViewName = config('app_settings.setting_page_view');
+
+        return view($settingViewName, compact('settingsUI'));
+    }
+
+    /**
+     * Save settings
+     *
+     * @param Request $request
+     * @param AppSettings $appSettings
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request, AppSettings $appSettings)
+    {
+        // validate the settings
+        $this->validate($request, $appSettings->getValidationRules());
+
+        // save settings
+        $appSettings->save($request);
+
+        return redirect(config('app_settings.url', '/'))
+            ->with([
+                'status' => config('app_settings.submit_success_message', 'Settings Saved.')
+            ]);
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 Route::group([
-    'namespace' => 'QCod\AppSettings\Controllers',
     'middleware' => array_merge(['web'], config('app_settings.middleware', []))
 ], function () {
-    Route::get(config('app_settings.url'), 'AppSettingController@index');
-
-    Route::post(config('app_settings.url'), 'AppSettingController@store');
+    Route::get(config('app_settings.url'), config('app_settings.controller').'@index');
+    Route::post(config('app_settings.url'), config('app_settings.controller').'@store');
 });

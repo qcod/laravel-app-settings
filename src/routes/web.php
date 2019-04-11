98d967dbb0757a3a3bb3edbe86845bddebb8aef5
Mexion <dev@mexion.com>
2019-04-11 19:54:44 +0200

    fix: routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,6 @@
 Route::group([
     'middleware' => array_merge(['web'], config('app_settings.middleware', []))
 ], function () {
-    Route::get(config('app_settings.url'), config('app_settings.controller').'@index');
-    Route::post(config('app_settings.url'), config('app_settings.controller').'@store');
+    Route::get(config('app_settings.url'). '/{page}', config('app_settings.controller').'@index');
+    Route::post(config('app_settings.url'). '/{page}', config('app_settings.controller').'@store');
 });
